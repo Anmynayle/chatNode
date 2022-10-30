482ae87107bf8d74a54417d00796ac98ce7e3116
Anmy Moreno <nayle2701@gmail.com>
2022-10-29 21:35:48 -0400

    adminvalidate

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@
 require('dotenv').config()
 
 const config ={
-    port:  process.env.PORT || 9000,
+    port:  process.env.PORT || 7000,
     nodeEnv: process.env.NODE_ENV || 'development', //? DEsarrollo, testing, produccion
     jwtSecret: process.env.JWT_SECRET,
     db:{
